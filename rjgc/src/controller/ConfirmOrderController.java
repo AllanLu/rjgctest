271cb1f92b55d2205df0bfc5523f332938ef1b46
Allan <mac@192.168.0.103>
2017-05-03 19:46:27 +0800

    提交赵展小组完成的java文件，原文件改为old

--- rjgc/src/controller/ConfirmOrderController.java
+++ rjgc/src/controller/ConfirmOrderController.java
@@ -8,8 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import model.ProductModel;
+import model.UserModel;
+import service.ProductInfoService;
+import service.UserService;
 
 @WebServlet(urlPatterns = {"/confirmOrderController.do"})
 public class ConfirmOrderController extends HttpServlet {
@@ -17,8 +21,24 @@ public class ConfirmOrderController extends HttpServlet {
 			throws ServletException,IOException {
 		response.setContentType("text/html;charset=utf-8");
 		//确认支付控制器
+		
 		//获取用户信息
+		UserService userservice=new UserService();
+		HttpSession session = request.getSession();
+		UserModel user =userservice.getUserFromSession(session);
+		
 		//调用ProductInfoService的saveHistory(List<ProductModel> list)方法，将商品加入历史记录表
+		ProductInfoService productinfoservice=new ProductInfoService();
+		List<ProductModel> list = (List<ProductModel>)session.getAttribute("list");
+		productinfoservice.saveHistory(list);
+		
 		//将参数送到confirmOrder.jsp显示
+		response.sendRedirect("confirmOrder.jsp");
+		//response.setStatus(307); 
+		//response.setHeader("Location","confirmOrder.jsp");  
+	}
+	public void doGet(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException
+	{
+			doPost(request,response);
 	}
 }
