80245f76be1d8bde01404716a6319d2f253923a3
chenzhiyang <648269767@qq.com>
2017-05-17 16:01:18 +0800

    zhaozhanzu

--- rjgc/src/service/UserService.java
+++ rjgc/src/service/UserService.java
@@ -1,11 +1,14 @@
 package service;
 
+import javax.servlet.http.HttpSession;
+
 import model.UserModel;
 
 public class UserService {
-	public UserModel getUserFromSession(){
+	public UserModel getUserFromSession(HttpSession session){
 		UserModel user = new UserModel();
 		//从session中获取用户信息
+		user=(UserModel) session.getAttribute("user");
 		return user;
 	}
 }
